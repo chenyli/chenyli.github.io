cb84162b0527f45d800c0b717c0e61cfb6ed1985
chenyli <450340801@qq.com>
2019-01-24 14:07:39 +0800

    Add files via upload

---
+++ lottery_data.js
@@ -0,0 +1,286 @@
+var lottery_initial_datas = [
+    {
+        "nameen": "xiaopenglei",
+        "namezh": "肖鹏磊"
+    },
+
+
+    {
+        "nameen": "cuilesi",
+        "namezh": "崔乐思"
+    }
+    ,
+
+    {
+        "nameen": "sunweibin",
+        "namezh": "孙玮彬"
+    }
+    ,
+    {
+        "nameen": "limingyuan",
+        "namezh": "李明渊"
+    }
+    ,
+    {
+        "nameen": "zhaojiangting",
+        "namezh": "赵江婷"
+    }
+    ,
+    {
+        "nameen": "shuaibinyang",
+        "namezh": "帅斌杨"
+    }
+    ,
+    {
+        "nameen": "yangbing",
+        "namezh": "杨兵"
+    },
+    {
+        "nameen": "zhangshixian",
+        "namezh": "张世先"
+    },
+    {
+        "nameen": "liulina",
+        "namezh": "刘利娜"
+    },
+    {
+        "nameen": "yihuaiyuan",
+        "namezh": "衣怀远"
+    },
+    {
+        "nameen": "huangsai",
+        "namezh": "黄赛"
+    },
+    {
+        "nameen": "majingsen",
+        "namezh": "马景森"
+    },
+    {
+        "nameen": "chenli",
+        "namezh": "陈丽"
+    },
+    {
+        "nameen": "lilei",
+        "namezh": "李磊"
+    },
+    {
+        "nameen": "yinyong",
+        "namezh": "殷勇"
+    },
+    {
+        "nameen": "shenyulong",
+        "namezh": "申玉龙"
+    }
+    ,
+
+    {
+        "nameen": "chenlou",
+        "namezh": "陈楼"
+    },
+    {
+        "nameen": "duweipo",
+        "namezh": "杜魏坡"
+    }
+    ,
+    {
+        "nameen": "wangkang",
+        "namezh": "王康"
+    }
+    ,
+    {
+        "nameen": "zhenghuiyan",
+        "namezh": "郑慧艳"
+    },
+    {
+        "nameen": "caoxiaoning",
+        "namezh": "曹小宁"
+    },
+    {
+        "nameen": "menghaorun",
+        "namezh": "孟浩润"
+    },
+    {
+        "nameen": "yangluan",
+        "namezh": "杨璐安"
+    },
+    {
+        "nameen": "hexinyan",
+        "namezh": "贺鑫炎"
+    }
+    ,
+    {
+        "nameen": "hedongling",
+        "namezh": "何冬玲"
+    },
+    {
+        "nameen": "chengran",
+        "namezh": "程然"
+    },
+    {
+        "nameen": "wangjian",
+        "namezh": "王剑"
+    }
+    ,
+    {
+        "nameen": "changxiaoping",
+        "namezh": "常晓萍"
+    },
+
+    {
+        "nameen": "fanweidong",
+        "namezh": "范伟东"
+    }
+    ,
+    {
+        "nameen": "gaoziqiang",
+        "namezh": "高自强"
+    },
+    {
+        "nameen": "zhangrui",
+        "namezh": "张瑞"
+    }
+    ,
+    {
+        "nameen": "caomengjiao",
+        "namezh": "曹梦娇"
+    }
+    ,
+    {
+        "nameen": "yinguanzhong",
+        "namezh": "尹冠中"
+    },
+    {
+        "nameen": "miaotengfei",
+        "namezh": "苗腾飞"
+    }
+    ,
+    {
+        "nameen": "zhangjian",
+        "namezh": "张健"
+    }
+    ,
+    {
+        "nameen": "qinzhirong",
+        "namezh": "覃志荣"
+    }
+    ,
+    {
+        "nameen": "liqin",
+        "namezh": "李钦"
+    },
+    {
+        "nameen": "xiemanning",
+        "namezh": "谢曼宁"
+    },
+    {
+        "nameen": "yanjia",
+        "namezh": "严嘉"
+    },
+    {
+        "nameen": "yangkyuijiang",
+        "namezh": "杨魁江"
+    },
+    {
+        "nameen": "wangwei",
+        "namezh": "王维"
+    },
+    {
+        "nameen": "yanjianhua",
+        "namezh": "严建华"
+    }
+    ,
+    {
+        "nameen": "huangxiangling",
+        "namezh": "黄湘玲"
+    }
+    ,
+    {
+        "nameen": "wanghuiping",
+        "namezh": "王慧萍"
+    },
+    {
+        "nameen": "yandan",
+        "namezh": "严丹"
+    }
+    ,
+    {
+        "nameen": "heyanfang",
+        "namezh": "何艳芳"
+    }
+    ,
+    {
+        "nameen": "heqiuping",
+        "namezh": "何秋平"
+    },
+    {
+        "nameen": "chenkun",
+        "namezh": "陈堃"
+    },
+    {
+        "nameen": "sunwenliang",
+        "namezh": "苏文亮"
+    },
+    {
+        "nameen": "yuanfang",
+        "namezh": "袁芳"
+    },
+    {
+        "nameen": "liweishuang",
+        "namezh": "李维双"
+    },
+
+    {
+        "nameen": "songxumin",
+        "namezh": "宋旭敏"
+    },
+    {
+        "nameen": "wangxiangnan",
+        "namezh": "王向楠"
+    }, {
+        "nameen": "zhaozhenyu",
+        "namezh": "赵振宇"
+    }, {
+        "nameen": "wuyue",
+        "namezh": "吴跃"
+    }, {
+        "nameen": "xurang",
+        "namezh": "许让"
+    }, {
+        "nameen": "zhangxiaofang",
+        "namezh": "张晓芳"
+    }, {
+        "nameen": "zhaoguangxu",
+        "namezh": "赵广旭"
+    }, {
+        "nameen": "zhaoyingchao",
+        "namezh": "赵英超"
+    },
+    {
+        "nameen": "zhanghongjun",
+        "namezh": "张红军"
+    },
+    {
+        "nameen": "songwentian",
+        "namezh": "宋文天"
+    },
+
+];
+
+var award_config = {
+    "award01": 1,
+    "award02": 3,
+    "award03": 6,
+    "award04": 100//抽奖次数
+};
+
+// 初始化数据
+//初始化抽奖号
+if (!localStorage.getItem('lottery_initial')) {
+    var data_str = JSON.stringify(lottery_initial_datas);
+    localStorage.setItem('lottery_initial', data_str);
+}
+//初始化抽奖次数
+if (!localStorage.getItem('award_initial')) {
+    var award_str = JSON.stringify(award_config);
+    localStorage.setItem('award_initial', award_str);
+}
